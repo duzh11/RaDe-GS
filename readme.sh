cc9d14dbf23a0df39b7f700794b64fddfb636931
Du_NUDT <meterodu@163.com>
2024-09-03 22:33:45 +0800

    增加了可视化outputs

--- readme.sh
+++ readme.sh
@@ -24,6 +24,13 @@ pip install -e .
 # ============ train and eval ============ # 
 # ======================================== # 
 
+# DTU
+# scan24, scan37, scan40, scan55, scan63, scan65, scan69, scan83, 
+# scan97, scan105, scan106, scan110, scan114, scan118, scan122
+
+# TNT
+# Barn, Caterpillar, Courthouse, Ignatius, Meetingroom, Truck
+
 # +++++++++ DTU +++++++++ # 
 python train.py -s ../../Data/DTU/scan24 -m ../exps/full/DTU/scan24 -r 2 --use_decoupled_appearance
 python mesh_extract.py -s ../../Data/DTU/scan24 -m ../exps/full/DTU/scan24 -r 2
@@ -35,8 +42,11 @@ python metric.py -m ../exps/full/DTU/scan24 -f train
 # +++++++++ TNT +++++++++ # 
 python train.py -s ../../Data/TNT/Barn -m ../exps/full/TNT/Barn -r 2 --eval --use_decoupled_appearance
 python mesh_extract_tetrahedra.py -s ../../Data/TNT/Barn -m ../exps/full/TNT/Barn -r 2 --eval
-# python eval_tnt/run.py --dataset-dir <path to GT TNT dataset> --traj-path <path to preprocessed TNT COLMAP_SfM.log file> --ply-path <output folder>/recon.ply
+python eval_tnt/run.py --dataset-dir ../../Data/TNT_dataset/Barn --traj-path ../../Data/TNT_dataset/Barn/Barn_COLMAP_SfM.log --ply-path ../exps/full/TNT/Barn/recon_tetrahedra.ply
 
 python render.py -s ../../Data/TNT/Barn -m ../exps/full/TNT/Barn -r 2 --eval
-python metric.py -m ../exps/full/TNT/Barn  -f train
-python metric.py -m ../exps/full/TNT/Barn  -f test
+python metric.py -m ../exps/full/TNT/Barn -f train
+python metric.py -m ../exps/full/TNT/Barn -f test
+
+# ============ concat and vis ============ # 
+python vis_outputs.py -f 'train' -m ../exps/full/DTU/scan24 ../exps/full/DTU/scan37
